1885a5ad80cb6eff4bbd65f1be367e07df029261
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-04 20:09:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def speak_to_grandma(phrase)
   binding.pry
-  if phrase == "I LOVE YOU GRANDMA"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   
     #puts "NO, NOT SINCE 1938!"
