aa87c3652e9f2a000e15a890f27f5185a81035ca
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-04 19:52:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,28 @@
+phrase1 = "Hi grandma".downcase
+
+def speak_to_grandma
+  if  "I LOVE YOU GRANDMA".upcase
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif .upcase
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end-of-input
+  
+def speak_to_grandma
+  
+end
+    
+def speak_to_grandma
+ if == .downcase
+   puts "HUH?! SPEAK UP, SONNY!"
+ elsif == .upcase
+    
+   
+   
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
